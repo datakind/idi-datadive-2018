bc03c40f7f05cd1c47123bd5561b42b1861b234a
Jimmy Yu <jimmy.yu@percolate.com>
2018-06-24 10:51:46 -0400

    add DFI search terms in 2nd sheet of output file

--- flask_scraper_demo/app/routes.py
+++ flask_scraper_demo/app/routes.py
@@ -77,7 +77,7 @@ def run_scraper():
         master_df = master_df.reset_index(drop=True)
 
     if len(master_df) > 0:
-        table_builder = TableBuilder(master_df)
+        table_builder = TableBuilder(master_df, scraper_names)
         table_builder.save_df()
         table_html = table_builder.get_table_html()
 
