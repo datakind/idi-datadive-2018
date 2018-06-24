bbbce037270d015eed48529c7aab7f4867bcac2a
Eddy Hintze <eddy@hintze.co>
2018-06-24 09:29:31 -0400

    Added kfw scraper

---
+++ scrapers/kfw_scraper.py
@@ -0,0 +1,54 @@
+import requests
+import urllib.parse
+import pandas as pd
+from parsel import Selector
+
+
+def scrape_kfw(search_term):
+    return KfwScraper().scrape(search_term)
+
+
+class KfwScraper(object):
+
+    DFI_NAME = 'kfw'
+    URL_TEMPLATE = "https://www.kfw-entwicklungsbank.de/Internationale-Finanzierung/KfW-Entwicklungsbank/Projekte/Projektdatenbank/ipfz/do.haupia?query={term}&page=1&rows=100000&sortBy=relevance&sortOrder=desc&facet.filter.language=de&dymFailover=true"  # noqa
+
+    def scrape(self, search_term):
+        results = []
+
+        formatted_term = urllib.parse.quote_plus(search_term)
+        url = self.URL_TEMPLATE.format(term=formatted_term)
+        print('Scraping Results')
+
+        source = requests.get(url, verify=False).json()
+        results.extend(self._get_projects_on_page(source))
+
+        print('Completed Search for', search_term, '\n')
+
+        df = self._build_dataframe(results)
+        return df
+
+    @staticmethod
+    def _get_projects_on_page(data):
+        """Build project data"""
+
+        projects_on_page = []
+        projects = data.get('proxyDidYouMean', {}).get('response', {}).get('response', {}).get('docs', [])
+        if projects == []:
+            # Try a diferent key set
+            projects = data.get('response', {}).get('docs', [])
+
+        for project in projects:
+            project_name = project.get('title')[0]
+            link = project.get('link')
+            if link is not None:
+                link = 'https://www.kfw-entwicklungsbank.de/ipfz/Projektdatenbank' + link
+            projects_on_page.append([project_name, link])
+
+        return projects_on_page
+
+    def _build_dataframe(self, results):
+        df = pd.DataFrame(results, columns=['Project Name', 'URL'])
+        df['Status'] = None
+        df['DFI'] = self.DFI_NAME
+        return df
