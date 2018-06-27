8bf49e7c37777a81f5a14db7c2410966c565e560
John Winter <jmw446@gmail.com>
2018-06-27 18:54:16 -0400

    Update miga_scraper.py
    same as other file - don't need columns =  line - it is redundant

--- scrapers/miga_scraper.py
+++ scrapers/miga_scraper.py
@@ -30,7 +30,6 @@ class MigaScraper(object):
         df['DFI'] = self.DFI_NAME
         print('Completed Search for', search_term, '\n')
         if df.shape[0] > 0:
-            df.columns = ['Project Name', 'URL', 'Status', 'DFI']
             df = df[['Project Name', 'URL', 'Status', 'DFI']]
             return df
         else:
